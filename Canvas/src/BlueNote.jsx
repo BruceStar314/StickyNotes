ce3aa693ffe75569808ccd8aa03d562781451782
Bruce <js.python03@gmail.com>
2025-09-11 14:20:10 -0700

    ...
    ...

--- Canvas/src/BlueNote.jsx
+++ Canvas/src/BlueNote.jsx
@@ -1,10 +1,9 @@
 function Palette({ onAddNote }) {
+    const [color, setColor] = useState('lightblue')
     return (
         <div className="flex gap-1 mb-1">
-            <textarea onClick={() => onAddNote("lightblue")} className='w-20 h-20 bg-blue-400 cursor-pointer rounded-sm'></textarea>
+            <textarea onClick={() => onAddNote(color)} className='w-20 h-20 bg-blue-400 cursor-pointer rounded-sm'></textarea>
         </div>
     )
-
 }
-
-  export default Palette
+export default Palette
--- Canvas/src/Container.jsx
+++ Canvas/src/Container.jsx
@@ -1,15 +1,18 @@
 import BlueNote from "./BlueNote";
+import { useState } from "react";
 function Container() {
+    const [notes, setNotes] = useState([])
     return (
         <>
         <div className="flex justify-center items-start h-screen w-screen mt-14">
-            <BlueNote/>
+            <BlueNote onAddNote={note => setNotes([...notes, note])}/>
             <div className='flex  h-[70vh] w-[75vw] bg-white rounded-2xl ml-5'>
-                <p className='text-gray-400'>Click the blue box to add a note</p>
+                {notes.map((note, index) => (
+                    <div key={index} className='flex h-20 w-20 bg-blue-400 cursor-pointer rounded-sm'></div>
+                ))}
             </div>
         </div>
         </>
     )
 }
-
 export default Container
